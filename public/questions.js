d474a497b1c561d622d8945a82bed2ba3d45cd37
Jeff Liu <jeffliu@jeffs-mbp.lan>
2023-04-27 12:26:56 -0400

    First commit generated by chatgpt

---
+++ public/questions.js
@@ -0,0 +1,10 @@
+export const questions = [
+    'What is your favorite color?',
+    'What is your favorite food?',
+    'What is your favorite book?',
+    'What is your favorite movie?',
+    'What is your favorite song?',
+    'What is your favorite hobby?',
+    'What is your favorite vacation spot?'
+  ];
+  
